57df15a1310fbf3064c6e2335ebe6f153061d15c
Storplaey <45475485+Storplaey@users.noreply.github.com>
2019-02-13 12:17:07 +0900

    Stply (#8)
    * Update .classpath
    
    * Update board.html
    
    * clicker
    
    * seperation
    
    separate board.js
    
    * Update boardFunction.js

--- WebContent/js/board.js
+++ WebContent/js/board.js
@@ -1,49 +1,55 @@
- $(document).ready(function () {
-            var gameBoard = document.getElementById('gameBoard');
+$(document).ready(function () {
+    var gameBoard = document.getElementById('gameBoard');
 
-            if (gameBoard.getContext) {
-                var ctx = gameBoard.getContext('2d');
-                var maxLife = 5;
-                var life = maxLife;
+    if (gameBoard.getContext) {
+        var ctx = gameBoard.getContext('2d');
+        var maxLife = 5;
+        var life = maxLife;
 
-                // make cover
-                ctx.fillStyle = "rgb(100,200,100)";
-                ctx.fillRect(0, 0, 800, 600);
+        // make cover
+        ctx.fillStyle = "rgb(100,200,100)";
+        ctx.fillRect(0, 0, 800, 600);
 
-                // make position map
-                var map = [];
-                for (var x = 0; x < 800; x += 10) {
-                    for (var y = 0; y < 600; y += 10) {
-                        map.push(mapMaker(x, y, true));
-                    }
-                }
-
-                var trail1 = [pointMaker(200, 200), pointMaker(300, 200), pointMaker(300, 250), pointMaker(250,
-                    250), pointMaker(250, 300), pointMaker(450, 300), pointMaker(450, 400), pointMaker(
-                    200, 400)];
-
-                var trail2 = [pointMaker(200, 200), pointMaker(300, 100), pointMaker(400, 200), pointMaker(300,
-                    300)];
-
-                // remove cover
-                discover(ctx, trail2, map);
-
-                // make score
-                $('#score').text(scoreCheck(map))
-
-                // show life
-                for (var i = 0; i < maxLife; i++) {
-                    if (i < life)
-                        $('#life').append('<img src="img/heart.png" width="20" height="20">');
-                    else
-                        $('#life').append('<img src="img/broken_heart.png" width="20" height="20">');
-                }
-                
-                $('#gameBoard').mousedown(function(event){
-                    x = event.pageX;
-                    y = event.pageY;
-                    console.log(x + ", " + y)
-                    //asdfasdf 
-                })
+        // make position map
+        var map = [];
+        for (var x = 50; x < 850; x += 10) {
+            for (var y = 50; y < 650; y += 10) {
+                map.push(mapMaker(x, y, true));
             }
-        });
+        }
+
+        var trail1 = [pointMaker(200, 200), pointMaker(300, 200), pointMaker(300, 250), pointMaker(250,
+            250), pointMaker(250, 300), pointMaker(450, 300), pointMaker(450, 400), pointMaker(
+            200, 400)];
+
+        var trail2 = [pointMaker(200, 200), pointMaker(300, 100), pointMaker(400, 200), pointMaker(300,
+            300)];
+
+        // remove cover
+        // discover(ctx, trail2, map);
+
+        // make score
+        $('#score').text(scoreCheck(map))
+
+        // show life
+        for (var i = 0; i < maxLife; i++) {
+            if (i < life)
+                $('#life').append('<img src="img/heart.png" width="20" height="20">');
+            else
+                $('#life').append('<img src="img/broken_heart.png" width="20" height="20">');
+        }
+
+        $('#gameBoard').mousedown(function (event) {
+            x = event.pageX - 50;
+            y = event.pageY - 50;
+
+            x = x - x % 10;
+            y = y - y % 10;
+
+            // console.log(x + ", " + y)
+            // console.log('map : ' + map[mapFinder(map, y, y)].x + ", " + map[mapFinder(map, y, y)].y)
+            // console.log('index : ' + mapFinder(map, y, y))
+            discoverPoint(ctx, x, y, map);
+        })
+    }
+});
